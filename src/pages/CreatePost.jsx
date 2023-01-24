671c3a19f128370ea5b1fc6c20b39250dc35f62a
AbdullahAnsariskylinx <abd45015@gmail.com>
2023-01-24 18:48:57 +0500

    url

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -18,7 +18,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch(`${process.env.REACT_APP_BASE_UR}/api/v1/dalle`,{
+        const response = await fetch(`${process.env.REACT_APP_BASE_URL}/api/v1/dalle`,{
           method:'POST',
           headers:{
             'Content-Type':'application/json',
@@ -44,7 +44,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch(`${process.env.REACT_APP_BASE_UR}/api/v1/post`,{
+        const response = await fetch(`${process.env.REACT_APP_BASE_URL}/api/v1/post`,{
           method:'POST',
           headers:{
             'Content-Type':'application/json',
