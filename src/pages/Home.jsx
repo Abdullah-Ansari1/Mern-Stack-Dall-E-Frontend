f54ad9d2a0b099eed5cd1f5563edbdcfc2877e86
AbdullahAnsariskylinx <abd45015@gmail.com>
2023-01-24 18:45:39 +0500

    Some Changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
     const fetchPosts = async () => {
       setLoading(true);
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch(`${process.env.REACT_APP_BASE_UR}/api/v1/post`, {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
