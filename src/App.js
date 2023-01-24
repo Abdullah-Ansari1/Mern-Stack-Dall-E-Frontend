d848237b84e19331dc231bfa5f30c8f2c88440a1
AbdullahAnsariskylinx <abd45015@gmail.com>
2023-01-24 17:57:56 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import {BrowserRouter,Link,Route,Routes} from 'react-router-dom';
+import {logo} from './assets';
+import {Home,CreatePost} from './pages';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ <BrowserRouter>
+ <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
+<Link to='/'>
+<img src={logo} alt="logo" className="w-28 object-contain"/>
+</Link>
+<Link to='/create-post' className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>Create</Link>
+ </header>
+ <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
+<Routes>
+<Route path='/' element={<Home/>}/>
+<Route path='/create-post' element={<CreatePost/>}/>
+
+</Routes>
+ </main>
+ </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
